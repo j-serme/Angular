d202a84f52d91e6c882992499b3d19ea7741fbc3
Julien Serme <julienserme@yahoo.com>
2021-12-10 11:26:48 +0100

    Exercice 3

---
+++ exam/src/app/models/statistique.ts
@@ -0,0 +1,11 @@
+export class Statistique {
+    identifiant: string
+    titre: string
+    valeur: string
+
+    constructor(identifiant: string, titre: string, valeur: string) {
+        this.identifiant = identifiant
+        this.titre = titre
+        this.valeur = valeur
+    }
+}
