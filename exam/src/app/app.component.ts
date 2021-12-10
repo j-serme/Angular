0554707e72a6912c2341902959022fe5c5cc25c7
Julien Serme <julienserme@yahoo.com>
2021-12-10 11:34:48 +0100

    Exercice 4

--- exam/src/app/app.component.ts
+++ exam/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Statistique } from './models/statistique';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'exam';
+
+  stat1 = new Statistique("fa1free0-be3b-11mb-58ec-7f4963ecfb46", "Démographie en Espagne", "45M");
+  stat2 = new Statistique("fa1free0-be3b-11mb-58ec-7f4963ecfb46", "Nombre de morts en Finlande en 2020", "100K");
 }
